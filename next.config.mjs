8880538214bb1c0ce744a374cec4e40950998da0
mrtnpro <github@mrtn.pro>
2024-11-26 17:06:39 +0100

    Remove standalone

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,6 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  productionBrowserSourceMaps: true,
   experimental: {
     typedRoutes: true,
   },
